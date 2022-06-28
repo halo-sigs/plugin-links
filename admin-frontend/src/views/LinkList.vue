51cbfc3291928969813beef111275fb8a0634146
Ryan Wang <i@ryanc.cc>
2022-06-28 16:19:14 +0800

    feat: add image loading state
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- admin-frontend/src/views/LinkList.vue
+++ admin-frontend/src/views/LinkList.vue
@@ -3,6 +3,7 @@ import { onMounted, ref, watch } from "vue";
 import Draggable from "vuedraggable";
 import {
   IconAddCircle,
+  IconInformation,
   IconList,
   IconSettings,
   VButton,
@@ -16,6 +17,7 @@ import { axiosInstance } from "@halo-dev/admin-shared";
 import type { Link, LinkGroup } from "@/types/extension";
 import yaml from "yaml";
 import { useFileSystemAccess } from "@vueuse/core";
+import { UseImage } from "@vueuse/components";
 
 const drag = ref(false);
 const links = ref<Link[]>();
@@ -393,13 +395,35 @@ onMounted(() => {
                     </div>
                     <div v-if="link.spec.logo" class="mr-4">
                       <div
-                        class="h-12 w-12 overflow-hidden rounded border bg-white hover:shadow-sm"
+                        class="inline-flex h-12 w-12 items-center justify-center overflow-hidden rounded border bg-white hover:shadow-sm"
                       >
-                        <img
-                          :alt="link.metadata.name"
-                          :src="link.spec.logo"
-                          class="h-full w-full"
-                        />
+                        <UseImage :src="link.spec.logo">
+                          <template #loading>
+                            <svg
+                              class="h-5 w-5 animate-spin"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <circle
+                                class="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                stroke-width="4"
+                              ></circle>
+                              <path
+                                class="opacity-75"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                fill="currentColor"
+                              ></path>
+                            </svg>
+                          </template>
+                          <template #error>
+                            <IconInformation class="h-5 w-5" />
+                          </template>
+                        </UseImage>
                       </div>
                     </div>
                     <div class="flex-1">
