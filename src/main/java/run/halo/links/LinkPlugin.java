c250cde174e5a19070687d8627ab948d7a4449b4
guqing <38999863+guqing@users.noreply.github.com>
2024-05-30 14:27:14 +0800

    refactor: use index mechanism to query links and groups (#65)
    ### What this PR does?
    使用索引机制查询链接和分组
    
    /kind improvement
    
    Fixes #64, #60
    
    ```release-note
    使用索引机制查询链接和分组
    ```

--- src/main/java/run/halo/links/LinkPlugin.java
+++ src/main/java/run/halo/links/LinkPlugin.java
@@ -1,9 +1,14 @@
 package run.halo.links;
 
-import org.pf4j.PluginWrapper;
+import static run.halo.app.extension.index.IndexAttributeFactory.simpleAttribute;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import run.halo.app.extension.Scheme;
 import run.halo.app.extension.SchemeManager;
+import run.halo.app.extension.index.IndexSpec;
 import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
 
 /**
  * @author guqing
@@ -14,20 +19,45 @@ public class LinkPlugin extends BasePlugin {
 
     private final SchemeManager schemeManager;
 
-    public LinkPlugin(PluginWrapper wrapper, SchemeManager schemeManager) {
-        super(wrapper);
+    public LinkPlugin(PluginContext pluginContext, SchemeManager schemeManager) {
+        super(pluginContext);
         this.schemeManager = schemeManager;
     }
 
     @Override
     public void start() {
-        schemeManager.register(Link.class);
-        schemeManager.register(LinkGroup.class);
+        schemeManager.register(Link.class, indexSpecs -> {
+            indexSpecs.add(new IndexSpec()
+                .setName("spec.displayName")
+                .setIndexFunc(simpleAttribute(Link.class, link -> link.getSpec().getDisplayName()))
+            );
+            indexSpecs.add(new IndexSpec()
+                .setName("spec.description")
+                .setIndexFunc(simpleAttribute(Link.class, link -> link.getSpec().getDescription()))
+            );
+            indexSpecs.add(new IndexSpec()
+                .setName("spec.groupName")
+                .setIndexFunc(simpleAttribute(Link.class, link -> {
+                    var group = link.getSpec().getGroupName();
+                    return StringUtils.isBlank(group) ? null : group;
+                }))
+            );
+            indexSpecs.add(new IndexSpec()
+                .setName("spec.priority")
+                .setIndexFunc(simpleAttribute(Link.class, link -> String.valueOf(link.getSpec().getPriority())))
+            );
+        });
+        schemeManager.register(LinkGroup.class, indexSpecs -> {
+            indexSpecs.add(new IndexSpec()
+                .setName("spec.priority")
+                .setIndexFunc(simpleAttribute(LinkGroup.class, group -> String.valueOf(group.getSpec().getPriority())))
+            );
+        });
     }
 
     @Override
     public void stop() {
-        schemeManager.unregister(schemeManager.get(Link.class));
-        schemeManager.unregister(schemeManager.get(LinkGroup.class));
+        schemeManager.unregister(Scheme.buildFromType(Link.class));
+        schemeManager.unregister(Scheme.buildFromType(LinkGroup.class));
     }
 }
--- src/main/java/run/halo/links/LinkRouter.java
+++ src/main/java/run/halo/links/LinkRouter.java
@@ -1,42 +1,40 @@
 package run.halo.links;
 
-import static java.util.Comparator.comparing;
+import static org.springdoc.core.fn.builders.parameter.Builder.parameterBuilder;
+import static org.springframework.data.domain.Sort.Order.asc;
+import static org.springframework.data.domain.Sort.Order.desc;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static run.halo.app.extension.router.QueryParamBuildUtil.buildParametersFromType;
-import static run.halo.app.extension.router.selector.SelectorUtil.labelAndFieldSelectorToPredicate;
+import static run.halo.app.extension.index.query.QueryFactory.*;
+import static run.halo.app.extension.router.selector.SelectorUtil.labelAndFieldSelectorToListOptions;
 
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.function.Predicate;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springdoc.core.fn.builders.operation.Builder;
 import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
-import org.springframework.util.comparator.Comparators;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-import run.halo.app.core.extension.endpoint.SortResolver;
-import run.halo.app.extension.Extension;
+import run.halo.app.extension.ListOptions;
 import run.halo.app.extension.ListResult;
 import run.halo.app.extension.ReactiveExtensionClient;
 import run.halo.app.extension.router.IListRequest;
+import run.halo.app.extension.router.SortableRequest;
+import run.halo.app.extension.router.selector.FieldSelector;
 import run.halo.links.finders.LinkFinder;
 import run.halo.links.vo.LinkGroupVo;
 
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class LinkRouter {
 
@@ -69,7 +67,7 @@ public class LinkRouter {
                     builder.operationId("listLinks")
                         .description("Lists link by query parameters")
                         .tag(tag);
-                    buildParametersFromType(builder, LinkQuery.class);
+                    LinkQuery.buildParameters(builder);
                 }
             ).build();
     }
@@ -81,19 +79,13 @@ public class LinkRouter {
     }
 
     private Mono<ListResult<Link>> listLink(LinkQuery query) {
-        return client.list(Link.class, query.toPredicate(),
-            query.toComparator(),
-            query.getPage(),
-            query.getSize()
-        );
+        return client.listBy(Link.class, query.toListOptions(), query.toPageRequest());
     }
 
-    static class LinkQuery extends IListRequest.QueryListRequest {
-        private final ServerWebExchange exchange;
+    static class LinkQuery extends SortableRequest {
 
         public LinkQuery(ServerWebExchange exchange) {
-            super(exchange.getRequest().getQueryParams());
-            this.exchange = exchange;
+            super(exchange);
         }
 
         @Schema(description = "Keyword to search links under the group")
@@ -106,80 +98,48 @@ public class LinkRouter {
             return queryParams.getFirst("groupName");
         }
 
-        @ArraySchema(uniqueItems = true,
-            arraySchema = @Schema(name = "sort",
-                description = "Sort property and direction of the list result. Supported fields: "
-                    + "creationTimestamp, priority"),
-            schema = @Schema(description = "like field,asc or field,desc",
-                implementation = String.class,
-                example = "creationTimestamp,desc"))
+        @Override
+        public ListOptions toListOptions() {
+            var listOptions = labelAndFieldSelectorToListOptions(getLabelSelector(), getFieldSelector());
+            var query = listOptions.getFieldSelector().query();
+            if (StringUtils.isNotBlank(getKeyword())) {
+                query = and(query, or(
+                    contains("spec.displayName", getKeyword()),
+                    contains("spec.description", getKeyword())
+                ));
+            }
+
+            if (StringUtils.isNotBlank(getGroupName())) {
+                query = and(query, equal("spec.groupName", getGroupName()));
+            }
+            listOptions.setFieldSelector(FieldSelector.of(query));
+            return listOptions;
+        }
+
+        @Override
         public Sort getSort() {
-            return SortResolver.defaultInstance.resolve(exchange);
+            return super.getSort()
+                .and(Sort.by(desc("metadata.creationTimestamp"),
+                    asc("metadata.name"))
+                );
         }
 
-        public Predicate<Link> toPredicate() {
-            Predicate<Link> keywordPredicate = link -> {
-                var keyword = getKeyword();
-                if (StringUtils.isBlank(keyword)) {
-                    return true;
-                }
-                String keywordToSearch = keyword.trim().toLowerCase();
-                return StringUtils.containsAnyIgnoreCase(link.getSpec().getDisplayName(),
-                    keywordToSearch)
-                    || StringUtils.containsAnyIgnoreCase(link.getSpec().getDescription(),
-                    keywordToSearch)
-                    || StringUtils.containsAnyIgnoreCase(link.getSpec().getUrl(), keywordToSearch);
-            };
-            Predicate<Link> groupPredicate = link -> {
-                var groupName = getGroupName();
-                if (StringUtils.isBlank(groupName)) {
-                    return true;
-                }
-                return StringUtils.equals(groupName, link.getSpec().getGroupName());
-            };
-            Predicate<Extension> labelAndFieldSelectorToPredicate =
-                labelAndFieldSelectorToPredicate(getLabelSelector(), getFieldSelector());
-            return groupPredicate.and(keywordPredicate).and(labelAndFieldSelectorToPredicate);
-        }
-
-        public Comparator<Link> toComparator() {
-            var sort = getSort();
-            var ctOrder = sort.getOrderFor("creationTimestamp");
-            var priorityOrder = sort.getOrderFor("priority");
-            List<Comparator<Link>> comparators = new ArrayList<>();
-            if (ctOrder != null) {
-                Comparator<Link> comparator =
-                    comparing(link -> link.getMetadata().getCreationTimestamp());
-                if (ctOrder.isDescending()) {
-                    comparator = comparator.reversed();
-                }
-                comparators.add(comparator);
-            }
-            if (priorityOrder != null) {
-                Comparator<Link> comparator =
-                    comparing(link -> link.getSpec().getPriority(),
-                        Comparators.nullsLow());
-                if (priorityOrder.isDescending()) {
-                    comparator = comparator.reversed();
-                }
-                comparators.add(comparator);
-            }
-            comparators.add(compareCreationTimestamp(false));
-            comparators.add(compareName(true));
-            return comparators.stream()
-                .reduce(Comparator::thenComparing)
-                .orElse(null);
-        }
-
-        public static <E extends Extension> Comparator<E> compareCreationTimestamp(boolean asc) {
-            var comparator =
-                Comparator.<E, Instant>comparing(e -> e.getMetadata().getCreationTimestamp());
-            return asc ? comparator : comparator.reversed();
-        }
-
-        public static <E extends Extension> Comparator<E> compareName(boolean asc) {
-            var comparator = Comparator.<E, String>comparing(e -> e.getMetadata().getName());
-            return asc ? comparator : comparator.reversed();
+        public static void buildParameters(Builder builder) {
+            IListRequest.buildParameters(builder);
+            builder.parameter(parameterBuilder()
+                    .name("keyword")
+                    .description("Keyword to search links under the group")
+                    .in(ParameterIn.QUERY)
+                    .implementation(String.class)
+                    .required(false)
+                )
+                .parameter(parameterBuilder()
+                    .name("groupName")
+                    .description("Link group name")
+                    .in(ParameterIn.QUERY)
+                    .implementation(String.class)
+                    .required(false)
+                );
         }
     }
 
