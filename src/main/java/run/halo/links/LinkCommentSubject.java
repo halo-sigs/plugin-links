d3b707c800867674c5ddfca508157b0188a2436d
John Niang <johnniang@foxmail.com>
2026-01-05 11:38:31 +0800

    Fix the problem of ref without version

--- src/main/java/run/halo/links/LinkCommentSubject.java
+++ src/main/java/run/halo/links/LinkCommentSubject.java
@@ -1,5 +1,6 @@
 package run.halo.links;
 
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -16,29 +17,29 @@ import run.halo.app.plugin.PluginContext;
  */
 @Component
 @RequiredArgsConstructor
-public class LinkCommentSubject implements CommentSubject<Plugin> {
-    
+class LinkCommentSubject implements CommentSubject<Plugin> {
+
     private final ReactiveExtensionClient client;
-    
+
     private final PluginContext pluginContext;
-    
+
+    private final GroupVersionKind gvk = GroupVersionKind.fromExtension(Plugin.class);
+
     @Override
     public Mono<Plugin> get(String name) {
         return client.get(Plugin.class, name);
     }
-    
+
     @Override
     public Mono<SubjectDisplay> getSubjectDisplay(String name) {
         return Mono.just(new SubjectDisplay("链接", "/links", "链接"));
     }
-    
+
     @Override
     public boolean supports(Ref ref) {
         Assert.notNull(ref, "Subject ref must not be null.");
-        GroupVersionKind groupVersionKind = new GroupVersionKind(ref.getGroup(),
-            ref.getVersion(), ref.getKind()
-        );
-        return GroupVersionKind.fromExtension(Plugin.class).equals(
-            groupVersionKind) && pluginContext.getName().equals(ref.getName());
+        return Objects.equals(gvk.group(), ref.getGroup())
+            && Objects.equals(gvk.kind(), ref.getKind())
+            && Objects.equals(pluginContext.getName(), ref.getName());
     }
 }
