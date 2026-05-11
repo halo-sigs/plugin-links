ffd5df24f963e86dad73b8c16c17e740cc01c223
困困鱼 <89380218+chengzhongxue@users.noreply.github.com>
2026-05-11 22:54:16 +0800

    Add public API (#119)
    ### 新增 公开 API
    
    /apis/api.link.halo.run/v1alpha1/links  GET  查询列表链接 filters (group, keyword, labelSelector, fieldSelector, sort, page, size)
    /apis/api.link.halo.run/v1alpha1/link-random GET  查询随机数链接 filters (maxSize)
    /apis/api.link.halo.run/v1alpha1/link-count GET 查询链接总数
    /apis/api.link.halo.run/v1alpha1/linkgroups GET 查询链接分组 
    
    ### 新增 Finder API
    random filters (maxSize) 查询随机数链接
    count 查询链接总数
    
    ```release-note
    添加公开 API
    ```

---
+++ src/main/java/run/halo/links/LinkGroupQueryEndpoint.java
@@ -0,0 +1,54 @@
+package run.halo.links;
+
+import static org.springdoc.core.fn.builders.apiresponse.Builder.responseBuilder;
+import static org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+import run.halo.app.core.extension.endpoint.CustomEndpoint;
+import run.halo.app.extension.GroupVersion;
+import run.halo.app.extension.ListResult;
+import run.halo.links.finders.LinkPublicQueryService;
+import run.halo.links.vo.LinkGroupVo;
+
+/**
+ * Public endpoint for link group queries.
+ */
+@Component
+@RequiredArgsConstructor
+public class LinkGroupQueryEndpoint implements CustomEndpoint {
+
+    private final LinkPublicQueryService linkPublicQueryService;
+
+    @Override
+    public RouterFunction<ServerResponse> endpoint() {
+        final var tag = "api.link.halo.run/v1alpha1/LinkGroup";
+        return route()
+            .GET("linkgroups", this::listGroups,
+                builder -> builder.operationId("queryLinkGroups")
+                    .description("List link groups.")
+                    .tag(tag)
+                    .response(responseBuilder()
+                        .implementation(ListResult.generateGenericClass(LinkGroupVo.class)))
+            )
+            .build();
+    }
+
+    private Mono<ServerResponse> listGroups(ServerRequest request) {
+        LinkPublicQuery query = new LinkPublicQuery(request.exchange());
+        return linkPublicQueryService.listGroups(query.toListOptions(), query.toPageRequest())
+            .flatMap(result -> ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(result));
+    }
+
+    @Override
+    public GroupVersion groupVersion() {
+        return GroupVersion.parseAPIVersion("api.link.halo.run/v1alpha1");
+    }
+}
---
+++ src/main/java/run/halo/links/finders/LinkPublicQueryService.java
@@ -0,0 +1,35 @@
+package run.halo.links.finders;
+
+import reactor.core.publisher.Mono;
+import run.halo.app.extension.ListOptions;
+import run.halo.app.extension.ListResult;
+import run.halo.app.extension.PageRequest;
+import run.halo.links.vo.LinkGroupVo;
+import run.halo.links.vo.LinkVo;
+import java.util.List;
+
+public interface LinkPublicQueryService {
+
+    /**
+     * List links with filters and pagination.
+     *
+     * @param options list options
+     * @param page    page request
+     * @return a mono of list result
+     */
+    Mono<ListResult<LinkVo>> listLinks(ListOptions options, PageRequest page);
+
+    /**
+     * List link groups without inline photos.
+     *
+     * @param options list options
+     * @param page    page request
+     * @return a mono of list result
+     */
+    Mono<ListResult<LinkGroupVo>> listGroups(ListOptions options, PageRequest page);
+
+
+    Mono<List<LinkVo>> random(Integer maxSize);
+
+    Mono<Integer> count();
+}
